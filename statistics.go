21e7a7954a954cfe09c3eb6c29b312faf6bcbffd
Mitesh Singh Jat <mitesh.singh.jat@gmail.com>
2023-07-16 23:51:05 +0530

    Added Quantile

--- statistics.go
+++ statistics.go
@@ -31,11 +31,27 @@ func Median(numbers ...float64) float64 {
 	sort.Float64s(dataCopy)
 
 	var median float64
+	var mid int = l / 2
 	if l%2 == 0 {
-		median = (dataCopy[l/2-1] + dataCopy[l/2]) / 2
+		median = (dataCopy[mid-1] + dataCopy[mid]) / 2
 	} else {
-		median = dataCopy[l/2]
+		median = dataCopy[mid]
 	}
 
 	return median
+}
+
+// Find the pth-percentile value in numbers
+func Quantile(p float32, numbers ...float64) float64 {
+	l := len(numbers)
+	if l == 0 {
+		return 0
+	}
+	dataCopy := make([]float64, l)
+	copy(dataCopy, numbers)
+
+	sort.Float64s(dataCopy)
+	pIndex := int(p * float32(l))
+	sort.Float64s(dataCopy)
+	return dataCopy[pIndex]
 }
--- statistics_test.go
+++ statistics_test.go
@@ -26,4 +26,29 @@ func TestMedian(t *testing.T) {
 	if ret != 0.0 {
 		t.Fail()
 	}
+
+	ret = Median(1, 2, 3)
+	if ret != 2 {
+		t.Fail()
+	}
+
+	ret = Median(1, 2, 3, 4)
+	if ret != 2.5 {
+		t.Fail()
+	}
+}
+
+func TestQuantil(t *testing.T) {
+	ret := Quantile(0)
+	if ret != 0.0 {
+		t.Fail()
+	}
+	ret = Quantile(0.8, 3, 3, 3, 3, 2, 22, 2, 2, 2, 2, 1, 1, 1, 1, 1, 111)
+	if ret != 3 {
+		t.Errorf("Quantile ret = %f\n", ret)
+	}
+	ret = Quantile(0.9, 3, 3, 3, 3, 2, 22, 2, 2, 2, 2, 1, 1, 1, 1, 1, 111)
+	if ret != 22 {
+		t.Errorf("Quantile ret = %f\n", ret)
+	}
 }
