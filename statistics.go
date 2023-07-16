34ab5168d766e7847c7192a0042d2f32da365297
Mitesh Singh Jat <mitesh.singh.jat@gmail.com>
2023-07-16 12:11:34 +0530

    Added Median function

--- statistics.go
+++ statistics.go
@@ -1,10 +1,41 @@
 package gomathi
 
+import (
+	"sort"
+)
+
+// Statistics:
 // Find the mean of the n numbers
 func Mean(numbers ...float64) float64 {
+	l := (float64)(len(numbers))
+	if l == 0 {
+		return 0
+	}
 	var sum float64 = 0
 	for _, n := range numbers {
 		sum += n
 	}
-	return sum / (float64)(len(numbers))
+	return sum / l
+}
+
+// Statistics:
+// Find the median of the n numbers
+func Median(numbers ...float64) float64 {
+	l := len(numbers)
+	if l == 0 {
+		return 0
+	}
+	dataCopy := make([]float64, l)
+	copy(dataCopy, numbers)
+
+	sort.Float64s(dataCopy)
+
+	var median float64
+	if l%2 == 0 {
+		median = (dataCopy[l/2-1] + dataCopy[l/2]) / 2
+	} else {
+		median = dataCopy[l/2]
+	}
+
+	return median
 }
--- statistics_test.go
+++ statistics_test.go
@@ -6,8 +6,24 @@ import (
 
 func TestMean(t *testing.T) {
 	ret := Mean(1, 2, 3, 4)
-
 	if ret != 2.50 {
+		t.Fail()
+	}
+
+	ret = Mean()
+	if ret != 0.0 {
+		t.Fail()
+	}
+}
+
+func TestMedian(t *testing.T) {
+	ret := Median(3, 3, 3, 3, 2, 22, 2, 2, 2, 2, 1, 1, 1, 1, 1, 111)
+	if ret != 2 {
+		t.Fail()
+	}
+
+	ret = Median()
+	if ret != 0.0 {
 		t.Fail()
 	}
 }
